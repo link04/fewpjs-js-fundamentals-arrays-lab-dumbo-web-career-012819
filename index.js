7bac00b23626594203c18bbf7583c50f48aef702
Maximo Bautista <max.bautista97@gmail.com>
2019-03-13 15:30:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 
-const drivers = [];
+
 drivers.push("Odie");
